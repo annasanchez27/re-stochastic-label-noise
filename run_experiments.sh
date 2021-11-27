fa8e3694a5b0a4ce419192d8602c0ae584ea2976
sebastiaanvergunst <sebastiaanvergunst@gmail.com>
2021-11-27 14:26:40 +0100

    Update run_experiments.sh

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for use_sln in True False; do
-  for dataset in "cifar10" "cifar100"; do
+  for dataset in "cifar10"; do  # "cifar100"
     for noise_mode in "symmetric" "asymmetric" "instance_dependent" "openset"; do
         python main.py --noise_rate 0.4 --dataset "${dataset}" --noise_mode "${noise_mode}" --use_sln "${use_sln}"
     done
