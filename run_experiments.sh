6193faf2be018a5f43014b4eb307dafc705a772f
sebastiaanvergunst <sebastiaanvergunst@gmail.com>
2021-12-01 20:48:40 +0100

    Update run_experiments.sh

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
-#for dataset in "cifar10" "cifar100"; do
-#  for use_sln in false true; do
-#    for noise_mode in "openset" "symmetric" "asymmetric" "instance_dependent"; do
-#      if [[ $use_sln =  true ]];
-#        then
-#            python main.py --noise_rate 0.4 --dataset "${dataset}" --noise_mode "${noise_mode}" --use_sln "${use_sln}"
-#        else
-#            python main.py --noise_rate 0.4 --dataset "${dataset}" --noise_mode "${noise_mode}"
-#        fi
-#    done
-#  done
-#done
+for use_sln in false true; do
+  for noise_mode in "openset" "symmetric" "asymmetric" "instance_dependent"; do
+    if [[ $use_sln =  true ]];
+      then
+          python main.py --noise_rate 0.4 --dataset "cifar10" --noise_mode "${noise_mode}" --use_sln "${use_sln}"
+      else
+          python main.py --noise_rate 0.4 --dataset "cifar10" --noise_mode "${noise_mode}"
+      fi
+  done
+done
 
-for dataset in "cifar100"; do
-  for use_sln in false true; do
-    for noise_mode in "openset" "symmetric" "asymmetric" "instance_dependent"; do
-      if [[ $use_sln =  true ]];
-        then
-            python main.py --noise_rate 0.4 --dataset "${dataset}" --noise_mode "${noise_mode}" --use_sln "${use_sln}"
-        else
-            python main.py --noise_rate 0.4 --dataset "${dataset}" --noise_mode "${noise_mode}"
-        fi
-    done
+for use_sln in false true; do
+  for noise_mode in "openset" "symmetric" "asymmetric" "instance_dependent"; do
+    if [[ $use_sln =  true ]];
+      then
+          python main.py --noise_rate 0.4 --dataset "cifar100" --noise_mode "${noise_mode}" --use_sln "${use_sln}"
+      else
+          python main.py --noise_rate 0.4 --dataset "cifar100" --noise_mode "${noise_mode}"
+      fi
   done
 done
